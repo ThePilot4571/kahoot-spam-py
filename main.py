7d461383cd31194de5255937f480260cdac718f9
ThePilot4571 <148016334+ThePilot4571@users.noreply.github.com>
2023-11-15 13:15:52 -0700

    Update main.py
    Added comments

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # enter desired values below
-code = 1872639
-bots = 3
+code = 1872639 # Kahoot game code
+bots = 3 # Bot count
 
 # main
 
@@ -12,7 +12,7 @@ stg = [0, 0]  # [index, total spaces after]
 z = "@"  # "‌"
 
 
-def space(name):
+def space(name): # Creating ZWNJ for putting the same username multiple times. Needs improvement
     global stg
     global z
     if stg[0] > len(name):
@@ -28,22 +28,20 @@ def space(name):
 
 
 def spam():
-    driver = webdriver.Chrome()
-    basename = input("Root name: ")
-    for i in range(bots):
-        driver.get(f"https://kahoot.it/?pin={code}")
+    driver = webdriver.Chrome() # Selenium setup
+    basename = input("Username for bots: ")
+    for i in range(bots): 
+        driver.get(f"https://kahoot.it/?pin={code}") # Use join link to make bot run faster than it would by typing the code in
         while True:
             try:
-                nicknameInput = driver.find_element(By.ID, "nickname")
+                nicknameInput = driver.find_element(By.ID, "nickname") # Refers to the input field for the username
                 nicknameInput.send_keys(space(basename))
-                driver.find_element("xpath", '//button[normalize-space()="OK, go!"]').click()
+                driver.find_element("xpath", '//button[normalize-space()="OK, go!"]').click() # Refers to the enter button
                 break
             except:
                 pass
-        driver.switch_to.new_window('tab')
+        driver.switch_to.new_window('tab') # New tab
 
-    driver.quit()
+    driver.quit() # REMOVE THIS LINE IF YOU WANT THE BOTS TO STAY IN AFTE JOINING. They will leave after about 20 seconds after the for loop terminates (Bots take a while to leave after the browser is closed.)
 
-
-for x in range(150):
-    print(space("test"))
+spam()
